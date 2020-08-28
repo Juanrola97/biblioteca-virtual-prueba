4cdd036cc3d6adba378f0cdf91eb9cd04b64c4a8
juanrola97 <juanrola97@gmail.com>
2020-08-28 17:29:32 -0500

    ajustes en el routing y los modulos creacion del componente inicio.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from 'src/app/index/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent
+    
   ],
   imports: [
     BrowserModule,
